07350efd16d699693db42cbd5bbf5689da2749f6
Patryk Krajnik <patryk.krajnik1999@gmail.com>
2022-05-23 17:59:41 +0200

    dupsko papieża

--- src/com.auto/Car.java
+++ src/com.auto/Car.java
@@ -1,5 +1,17 @@
 package com.auto;
 
 public class Car {
+    public boolean isRental;
+    public String brand;
+    public String productionDate;
+    public int mileage;
+
+    public Car(boolean isRental, String brand, String productionDate, int  mileage)
+    {
+        this.isRental = isRental;
+        this.brand = brand;
+        this.productionDate = productionDate;
+        this.mileage = mileage;
+    }
 }
 //https://www.tutussfunny.com/car-rentail-system-source-code-download/
