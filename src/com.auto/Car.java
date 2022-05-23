4133445e5d0630f39c482baef188417d30afb71c
Bogumił Piotrowski <bogumilp98@gmail.com>
2022-05-23 23:10:39 +0200

    Changes by patry

--- src/com.auto/Car.java
+++ src/com.auto/Car.java
@@ -3,13 +3,15 @@ package com.auto;
 public class Car implements java.io.Serializable {
     public boolean isRental;
     public String brand;
-    public String productionDate;
+    public String model;
+    public LocalDateTime productionDate;
     public int mileage;
 
-    public Car(String brand, String productionDate, int  mileage)
+    public Car(String brand,String model, LocalDateTime productionDate, int  mileage)
     {
         this.isRental = false;
         this.brand = brand;
+        this.model = model;
         this.productionDate = productionDate;
         this.mileage = mileage;
     }
--- src/com.auto/CarUserRelation.java
+++ src/com.auto/CarUserRelation.java
@@ -5,8 +5,7 @@ public class CarUserRelation implements java.io.Serializable {
     Car car;
     User user;
 
-
-    public CarUserRelation(Car car, User user) {
+    public CarUserRelation(Car car, User user, LocalDateTime rentalStart, LocalDateTime rentalEnd) {
         this.car = car;
         this.user = user;
     }
